d53bd38030987e8b8787fc5c620b1830ad8707ff
Agustín Balcázar <agus.b.reyes@gmail.com>
2024-09-15 13:24:59 -0600

    Se aplicó una forma redondeada al buscador de la vista principal

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyAppBars.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyAppBars.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.reply.ui.components
 
@@ -22,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
@@ -47,7 +33,10 @@ fun ReplySearchBar(modifier: Modifier = Modifier) {
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(16.dp).background(MaterialTheme.colorScheme.background),
+            .padding(16.dp).background(
+                MaterialTheme.colorScheme.background,
+                CircleShape
+            ),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
