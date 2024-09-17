10ed7239d0868a39804682570a379b99a06c2885
Agustín Balcázar <agus.b.reyes@gmail.com>
2024-09-17 00:10:01 -0600

    Reconfiguración de listas mutables, se corrigieron errores pequeños

--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -3,20 +3,16 @@ package com.example.basicstatecodelab
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
-
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.toMutableStateList
 
-import androidx.compose.runtime.toMutableStateList
-import androidx.lifecycle.ViewModel
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        StatefulCounter()
 
-class WellnessViewModel : ViewModel() {
-    private val _tasks = getWellnessTasks().toMutableStateList()
-    val tasks: List<WellnessTask>
-        get() = _tasks
-
-    fun remove(item: WellnessTask) {
-        _tasks.remove(item)
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
     }
 }
 
