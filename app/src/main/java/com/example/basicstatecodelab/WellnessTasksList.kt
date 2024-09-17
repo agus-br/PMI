a8e75598c4848af860c9d43f72a8e8a265f0d204
Agustín Balcázar <agus.b.reyes@gmail.com>
2024-09-17 00:14:08 -0600

    Migración de la lista al ViewModel

--- app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
@@ -3,7 +3,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.runtime.remember
 
 
 @Composable
