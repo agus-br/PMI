894f1a44683ea8cfc37e492468c3eafc79959e46
Agustín Balcázar <agus.b.reyes@gmail.com>
2024-09-17 00:00:24 -0600

    Configuración de listas mutables, los listitems ahora se pueden eliminar

--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -4,10 +4,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
 
-@Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        StatefulCounter()
-        WellnessTasksList()
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+class WellnessViewModel : ViewModel() {
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask>
+        get() = _tasks
+
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
     }
 }
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
